5f3d544816293b3260196ac73f15ec3e508cab4e
Shikhar Saxena <shikhar@Shikhars-MacBook-Air.local>
2024-03-17 00:58:44 +0530

    changed main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "ap-south-1"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-080e1f13689e07408" 
+  ami           = "ami-007020fd9c84e18c7" 
   instance_type = "t2.micro"
 
   tags = {
