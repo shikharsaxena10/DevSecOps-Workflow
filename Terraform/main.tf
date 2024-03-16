f76686901259ffb4f8b95f0de0062898092f98bb
Shikhar Saxena <shikhar@Shikhars-MacBook-Air.local>
2024-03-17 01:56:05 +0530

    key-pair

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "foo" {
   ami           = "ami-007020fd9c84e18c7" 
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-046e334f94d46f520"]
-  key_name = "ssh-key.pem"
+  key_name = "ssh-key"
 
   tags = {
     Name = "tf-example"
