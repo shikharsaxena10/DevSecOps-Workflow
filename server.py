15b7be2886502157653ab88389fcebd5176a42ed
Shikhar Saxena <shikhar@Shikhars-MacBook-Air.local>
2024-03-17 17:39:58 +0530

    added index.html

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
-from flask import Flask, request
- 
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
- 
+
 @app.route('/')
 def show_ip():
     ip = request.remote_addr
     reversed_ip = '.'.join(ip.split('.')[::-1])
-    originalIp = f"The original ip is: {ip}\n"
-    reversedIp = f"\nThe reversed ip is: {reversed_ip}"
-    final = originalIp + reversedIp
-    return final
+    return render_template('index.html', ip=ip, rev_ip=reversed_ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
